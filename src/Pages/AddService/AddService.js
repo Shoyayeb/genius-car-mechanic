dc7b90bae499f2e762d6fc4e61f86f46bf6353d1
Shoyayeb <sa.fine@Outlook.com>
2021-10-28 20:19:09 +0600

    mongodb fetch addedc

--- src/Pages/AddService/AddService.js
+++ src/Pages/AddService/AddService.js
@@ -4,11 +4,14 @@ import { useForm } from "react-hook-form";
 import './AddService.css';
 
 const AddService = () => {
-    const { register, handleSubmit } = useForm();
+    const { register, handleSubmit, reset } = useForm();
     const onSubmit = data => {
         axios.post('http://localhost:5000/services', data)
             .then(res => {
-                console.log(res.data);
+                if (res.data.insertedId) {
+                    alert('Added successfully')
+                    reset();
+                }
             })
         console.log(data)
     };
