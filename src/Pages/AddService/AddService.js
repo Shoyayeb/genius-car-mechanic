2e7672d00cb384d6d1aabc167fa46943ab9be553
Shoyayeb <sa.fine@Outlook.com>
2021-10-29 03:50:40 +0600

    heroku added

--- src/Pages/AddService/AddService.js
+++ src/Pages/AddService/AddService.js
@@ -1,35 +1,42 @@
-import axios from 'axios';
-import React from 'react';
+import axios from "axios";
+import React from "react";
 import { useForm } from "react-hook-form";
-import './AddService.css';
+import "./AddService.css";
 
 const AddService = () => {
-    const { register, handleSubmit, reset } = useForm();
-    const onSubmit = data => {
-        axios.post('http://localhost:5000/services', data)
-            .then(res => {
-                if (res.data.insertedId) {
-                    alert('Added successfully')
-                    reset();
-                }
-            })
-        console.log(data)
-    };
+  const { register, handleSubmit, reset } = useForm();
+  const onSubmit = (data) => {
+    axios
+      .post(
+        "http://https://nameless-fortress-33208.herokuapp.com/services",
+        data
+      )
+      .then((res) => {
+        if (res.data.insertedId) {
+          alert("Added successfully");
+          reset();
+        }
+      });
+    console.log(data);
+  };
 
-    return (
-        <div>
-            <h1>Add a Service</h1>
-            <div className="add-service">
-                <form onSubmit={handleSubmit(onSubmit)}>
-                    <input placeholder="name" {...register("name", { required: true, maxLength: 30 })} />
-                    <textarea placeholder="description" {...register("description")} />
-                    <input placeholder="price" type="number" {...register("price")} />
-                    <input placeholder="image url" {...register("img")} />
-                    <input type="submit" />
-                </form>
-            </div>
-        </div>
-    );
+  return (
+    <div>
+      <h1>Add a Service</h1>
+      <div className="add-service">
+        <form onSubmit={handleSubmit(onSubmit)}>
+          <input
+            placeholder="name"
+            {...register("name", { required: true, maxLength: 30 })}
+          />
+          <textarea placeholder="description" {...register("description")} />
+          <input placeholder="price" type="number" {...register("price")} />
+          <input placeholder="image url" {...register("img")} />
+          <input type="submit" />
+        </form>
+      </div>
+    </div>
+  );
 };
 
 export default AddService;
--- src/Pages/Home/Services/Services.js
+++ src/Pages/Home/Services/Services.js
@@ -1,28 +1,25 @@
-import React, { useEffect, useState } from 'react';
-import Service from '../Service/Service';
-import './Services.css';
+import React, { useEffect, useState } from "react";
+import Service from "../Service/Service";
+import "./Services.css";
 
 const Services = () => {
-    const [services, setServices] = useState([])
-    useEffect(() => {
-        fetch('http://localhost:5000/services')
-            .then(res => res.json())
-            .then(data => setServices(data));
-    }, [])
+  const [services, setServices] = useState([]);
+  useEffect(() => {
+    fetch("http://https://nameless-fortress-33208.herokuapp.com/services")
+      .then((res) => res.json())
+      .then((data) => setServices(data));
+  }, []);
 
-    return (
-        <div id="services">
-            <h2 className="text-primary mt-5">Our services</h2>
-            <div className="service-container">
-                {
-                    services.map(service => <Service
-                        key={service._id}
-                        service={service}
-                    ></Service>)
-                }
-            </div>
-        </div>
-    );
+  return (
+    <div id="services">
+      <h2 className="text-primary mt-5">Our services</h2>
+      <div className="service-container">
+        {services.map((service) => (
+          <Service key={service._id} service={service}></Service>
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default Services;
